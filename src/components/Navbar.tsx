a24c9a1a61d03da745b79662d3783e6df0ceb7f3
syhbandi <saboonlbk@gmail.com>
2024-12-16 07:35:56 +0800

    update app name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full">
       <div className="container mx-auto py-5 flex items-center justify-between">
-        <h2 className="font-semibold text-white text-2xl">Weatheria°</h2>
+        <h2 className="font-semibold text-white text-2xl">Weatharia°</h2>
       </div>
     </nav>
   );
